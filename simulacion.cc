3a740fed17d382e8d91997dca167a30919d3479a
Manuel <lolocdf@gmail.com>
2017-01-17 12:45:14 +0100

    Ya se han añadidos los nodos.

--- simulacion.cc
+++ simulacion.cc
@@ -25,5 +25,60 @@
 // Includes del programa
 
 // Definiciones
+NS_LOG_COMPONENT_DEFINE ("trabajo07");
+
+void simulacion ();
+
+using namespace ns3;
 
 // Funciones
+
+int main (int argc, char * argv[]){
+  Time::SetResolution (Time::US);
+  
+  // Número de nodos de la red en anillo
+  int nodos = 3;
+
+  simulacion();
+  
+  return 0;
+}
+
+void simulacion (int nodos) {
+  NS_LOG_FUNCTION_NOARGS();
+  // Nodos independintes
+  NodeContainer nodos;
+  nodos.Create(nodos);
+  
+  // Creamos los distintos enlaces punto a punto
+  NodeContainer p2pNodes[nodos];
+
+
+  // Instalamos el dispositivo en los nodos punto a punto
+  PointToPointHelper pointToPoint[nodos];
+  NetDeviceContainer p2pDevices;
+
+  for (int enlace = 0; enlace < nodos; enlace ++){
+    pointToPoint[enlace].SetDeviceAttribute ("DataRate",
+				     DataRateValue (DataRqqqqqw22wate("2Mbps")));
+    pointToPoint[enlace].SetChannelAttribute ("Delay",
+				      TimeValue (Time ("2ms")));
+    
+    p2pNodes[enlace].Add (nodos.Get(enlace));
+    
+    if (enlace==nodos-1) 
+      p2pNodes[enlace].Add (p2pNodes.Get(0));
+
+    else
+      p2pNodes[enlace].Add (p2pNodes.Get(enlace+1));
+    
+    p2pDevices[enlace] = pointToPoint.Install (p2pNodes[enlace]);
+  }
+
+  // Instalamos la pila TCP/IP en todos los nodos
+  InternetStackHelper stack;
+  stack.Install (p2pNodes.Get (0));
+  stack.Install (csmaNodes);
+
+  
+}
